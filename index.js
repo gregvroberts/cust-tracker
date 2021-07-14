5fd9a7609de34514e2dc25532a07a4323e727e07
Gregory Roberts <roberts.gregory@heb.com>
2021-07-14 17:16:26 -0500

    for FP

--- index.js
+++ index.js
@@ -10,32 +10,22 @@
 
 import express from 'express';
 import route from './src/routes'
-const winston = require('winston');
-
-// Setup our logger with winston
-const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    defaultMeta: { service: 'user-service' },
-    transports: [
-        //
-        // - Write all logs with level `error` and below to `error.log`
-        // - Write all logs with level `info` and below to `combined.log`
-        //
-        new winston.transports.File({filename: 'logs/error.log', level: 'error'}),
-        new winston.transports.File({ filename: 'logs/combined.log'}),
-    ],
-});
-
+import logger from './src/util/logger';
 //
-// If we're not in production, then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest })`
-//
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console( {
-        format: winston.format.simple(),
-    }));
-}
+// // Setup our logger with winston
+// const logger = winston.createLogger({
+//     level: 'info',
+//     format: winston.format.json(),
+//     defaultMeta: { service: 'user-service' },
+//     transports: [
+//         //
+//         // - Write all logs with level `error` and below to `error.log`
+//         // - Write all logs with level `info` and below to `combined.log`
+//         //
+//         new winston.transports.File({filename: 'logs/error.log', level: 'error'}),
+//         new winston.transports.File({ filename: 'logs/combined.log'}),
+//     ],
+// });
 
 const app = express(); // Create our app
 
